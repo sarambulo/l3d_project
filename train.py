86200343bd1c156ac47154327ba88fc6c306c000
sarambulo <asarambu@gmail.com>
2025-12-11 20:53:10 -0500

    Naming issue for GT

--- train.py
+++ train.py
@@ -184,7 +184,7 @@ def evaluate(dataloader, netPred, device, epoch, vis_dir: str) -> float:
             gif_filename_format = '{:d}_gt.gif'.format
             obj_filename_format = '{:d}_gt.obj'.format
             render_mesh(vertsGt[index], facesGt[index], output_dir / gif_filename_format(visualization_count), device=device)
-            save_obj(output_dir / obj_filename_format(visualization_count + index), vertices[index], faces[index])
+            save_obj(output_dir / obj_filename_format(visualization_count), vertices[index], faces[index])
             visualization_count +=1
 
         loss_shape += batch_loss.mean().item() / n_batch
